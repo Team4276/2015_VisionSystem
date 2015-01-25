c7ade33fb2f0aec70cf11b94af273d5c4bd8b872
acappon <andrew.cappon@gmail.com>
2015-01-25 01:07:41 -0800

    Add web pages to install/uninstall scripts

--- viking_mpg_streamer/install.sh
+++ viking_mpg_streamer/install.sh
@@ -1,3 +1,6 @@
 sudo cp /home/ubuntu/viking_mpg_streamer/dist/Release/GNU-Linux-x86/viking_mpg_streamer /usr/bin
 sudo cp /home/ubuntu/viking_mpg_streamer/dist/Release/GNU-Linux-x86/input_uvc.so /usr/lib
 sudo cp /home/ubuntu/viking_mpg_streamer/dist/Release/GNU-Linux-x86/output_http.so /usr/lib
+sudo mkdir /usr/bin/www
+sudo cp  /home/ubuntu/viking_mpg_streamer/dist/Release/GNU-Linux-x86/www/*.* /usr/bin/www
+
--- viking_mpg_streamer/uninstall.sh
+++ viking_mpg_streamer/uninstall.sh
@@ -1,3 +1,5 @@
 sudo rm /usr/bin/viking_mpg_streamer
 sudo rm /usr/lib/input_uvc.so
 sudo rm /usr/lib/output_http.so
+rm -rf /usr/bin/www
+
